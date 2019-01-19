bdc8e359673836cac2a12ce2d3d53b34ca9053b9
Vlad <vlad.polovyi@gmail.com>
2019-01-19 15:08:32 +0200

    Provided base forms for motoworld client authorization.

---
+++ src/app/components/item-info/item-info.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-info',
+  templateUrl: './item-info.component.html',
+  styleUrls: ['./item-info.component.styl']
+})
+export class ItemInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+ 
+
+}
