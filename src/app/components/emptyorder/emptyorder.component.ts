72d8fe8562511128d516c10aaf18bb39ccc9ad61
BLOCKMATERIAL <miroslavgrischenko@gmail.com>
2019-01-22 21:52:01 +0200

    Added components emptyorder.

---
+++ src/app/components/emptyorder/emptyorder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emptyorder',
+  templateUrl: './emptyorder.component.html',
+  styleUrls: ['./emptyorder.component.styl']
+})
+export class EmptyorderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
